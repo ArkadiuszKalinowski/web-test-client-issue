f8b73ac460e3d2c6be7b7dd3637b044c62c83762
akalinowski <arkadiusz.kalinowski@vonage.com>
2024-08-29 15:31:47 +0200

    Add ranges test

--- src/main/java/server/ServiceApp.java
+++ src/main/java/server/ServiceApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -24,7 +25,18 @@ class SampleController {
   Flux<DataBuffer> testEndpoint(@RequestHeader HttpHeaders headers,
                                 ServerHttpResponse response) {
     var responseData = "sample123";
-    response.getHeaders().setContentLength(6);
+    var responseHeaders = response.getHeaders();
+    responseHeaders.setContentLength(6);
+
+    if (isRangeRequest(headers)) {
+      responseHeaders.set(HttpHeaders.CONTENT_RANGE, "bytes 0-5/9");
+      response.setStatusCode(HttpStatus.PARTIAL_CONTENT);
+    }
+
     return Flux.just(new DefaultDataBufferFactory().wrap(responseData.getBytes()));
+  }
+
+  private static boolean isRangeRequest(HttpHeaders headers) {
+    return headers.containsKey(HttpHeaders.RANGE);
   }
 }
